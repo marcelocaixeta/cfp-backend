0f27a93fe3a801b5d8b785452692e169ca993a43
Marcelo Caixeta <marcelocaixeta@gmail.com>
2026-06-10 19:44:46 -0300

    Add Bitcoin address balance endpoint

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'bitcoin' => [
+        'explorer_url' => env('BITCOIN_EXPLORER_URL', 'https://blockstream.info/api'),
+        'wallet_address' => env('BITCOIN_WALLET_ADDRESS'),
+        'timeout' => env('BITCOIN_EXPLORER_TIMEOUT', 10),
+    ],
+
 ];
